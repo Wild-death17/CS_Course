d78547891a63b0023d3aef2f68baa845c0b61fb1
MosheCohen <131193639+Wild-death17@users.noreply.github.com>
2023-06-07 10:29:49 +0300

    Add files via upload

---
+++ 3LS/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace _3LS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Console.Write("enter txt :");
+
+            string str = Console.ReadLine();
+
+            str = str.Substring( 0 , str.LastIndexOf(str[0]) + 1 );
+
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+            */
+
+            /*
+            Console.WriteLine($"str is :{tar1(,) }" ); 
+
+            static void tar1(string str,string Tav){
+
+            if(Tav.Lentgh != 1)
+               return -1;
+
+            int idx1 = str.IndexOf(Tav);
+            int idx2 = str.LastIndexOf(Tav);
+            str = str.Substring( idx1 , idx2 );
+
+             return str;
+            }
+            */
+
+            /*
+            static void PrintHello()
+            {
+                Console.WriteLine("Hello!");
+            }
+            */
+
+            /*
+            Console.Write("Enter Price Amount :");
+            float FullPrice = float.Parse(Console.ReadLine());
+            float DiscountedPrice = FullPrice - FullPrice * 0.17;
+            float NewPrice = FullPrice - DiscountedPrice;
+            Console.WriteLine($"Price is : {NewPrice}");*/
+
+            /*
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GG(x)); 
+
+
+            int[] arr = { -11, 55, 123 };
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine(GG(arr[i]));
+            static bool GG(int x)
+            {
+              if(( x > 0 ) && (x < 100))
+                    return true;
+               return false;
+            } */
+
+            /*
+            if ( a < b)
+            {
+                c = a
+                a = b 
+                b = c
+            }
+
+            c = (a < b) ? b : a;
+
+            sml = Math.Min( a , b );
+            bgst= Math.Max(a, b);
+
+            if (a > b)
+                return a;
+            return b;
+            */
+
+        }
+    }
+}
