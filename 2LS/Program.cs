327bb64f78fc3886ca65d1ace6fa82463b98d94e
MosheCohen <131193639+Wild-death17@users.noreply.github.com>
2023-06-07 10:29:08 +0300

    Add files via upload

---
+++ 2LS/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace _2LS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Console.WriteLine("Enter a string of Text");
+            String txt = Console.ReadLine();
+            Console.WriteLine("Enter a Number");
+            int Num = int.Parse(Console.ReadLine());
+
+            if (Num % 2 == 0)
+            {
+                Console.WriteLine(txt.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(txt.ToLower());
+            }
+            */
+            /*
+            String Str = Console.ReadLine();
+            Console.WriteLine($"{Str[0]}{Str[1]}{Str[Str.Length - 2]  }{Str[Str.Length - 1]}");
+            */
+            /*
+            Console.Write("Enter a string :");
+            string str = Console.ReadLine();
+            str = str.Substring(0 , 1).ToUpper() + str.Substring(1);
+            Console.WriteLine(str);
+            */
+            /*
+            string str = Console.ReadLine();
+            string L = "e";
+            if (str.IndexOf(L) == -1)
+            {
+                Console.WriteLine("None");
+            }
+            else if (str.indexof(l) == str.lastindexof(l))
+            {
+                console.writeline("1");
+            }
+            else
+            {
+                console.writeline("more");
+            }
+            */
+            /*
+            string txt = "we are the so-called \"vikings\" from the north";
+            console.writeline(txt);
+            console.writeline();
+            */
+            /*
+            int num;
+            bool Gn = true;
+            do
+            {
+                Console.Write("Please Enter A 3 digit number : ");
+                num = int.Parse(Console.ReadLine());
+
+                if (
+                   102 < num
+                && num < 992
+                && (num / 10) % 2 == 0 
+                && (num % 10) == 2
+                && (num % 10) < (num / 10 % 10)
+                && (num % 10) + (num / 10 % 10) == (num / 10 / 10))
+                {
+                    Gn = false;
+                }
+
+            }
+            while (Gn);
+            Console.WriteLine("Your number can be a mail box!");
+            */
+
+        }
+    }
+}
