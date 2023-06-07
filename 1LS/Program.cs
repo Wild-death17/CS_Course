ac5b0baaba63282154c934c199db47cf6e4afeff
MosheCohen <131193639+Wild-death17@users.noreply.github.com>
2023-06-07 10:27:44 +0300

    Add files via upload

---
+++ 1LS/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1LS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //     int s1 = int.Parse(Console.ReadLine());
+            //     int s2 = int.Parse(Console.ReadLine());
+            //     Console.WriteLine(s1 + s2);
+
+            //     /*  לקלוט 3 מספרים ולהדפיס את הגדול */
+            //     Console.Write("num1:");
+            //     int n1 = int.Parse(Console.ReadLine());
+            //     Console.Write("num2:");
+            //     int n2 = int.Parse(Console.ReadLine());
+            //     Console.Write("num3:");
+            //     int n3 = int.Parse(Console.ReadLine());
+            //     int biggest = Math.Max(n3, Math.Max(n1, n2));
+            //     Console.WriteLine($"biggest of({n1},{n2},{n3})={biggest}");
+
+            //Console.Write("Hello, ");
+            //Console.Write(" World!");
+            //int num = 13;
+            //int lower = 3;
+            //int sum=num+lower;
+            //string str = "13" + lower;
+            //Console.WriteLine(num+str);
+            //Console.WriteLine("sum="+sum+"  str="+str);
+            //double dbl = (double)num / lower;
+            //Console.WriteLine("{0}+{1}={2}",num+lower,num,lower);
+            //Console.WriteLine(dbl);
+
+        }
+    }
+}
+
+
