99863e1e51fa8bd7598469b4e91c8787fe8e2392
MosheCohen <131193639+Wild-death17@users.noreply.github.com>
2023-06-07 10:32:08 +0300

    Add files via upload

---
+++ 5LS/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace _5LS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1 {
+            //     static bool firstlast(string str)
+            //     {
+            //         char a = str[0], b = str[(str.Length - 1)];
+            //         if (a == b)
+            //             return true;
+            //         return false;
+            //     }
+            // }
+            //2 {
+            //     int cnt = 0;
+            //     string[] arr = new string[4];
+            //     for (int i = 0; i < 4; i++)
+            //     {
+            //         Console.WriteLine("Please Enter A Word : ");
+            //         arr[i] = Console.ReadLine();
+            //     }
+            //     foreach (var i in arr)
+            //     {
+            //         if (firstlast(i))
+            //             cnt++;
+            //     }
+            //     Console.WriteLine(cnt);
+            // }
+            // 3 {
+            //     static bool ischarhere(string str, char a)
+            //     {
+            //         if (str.IndexOf(a) != str.LastIndexOf(a))
+            //             return true;
+            //         return false;
+            //     }
+            // }
+            // 4 {
+            //     static bool IsArreyRich(string[] str)
+            //     {
+            //         foreach (var i in str)
+            //         {
+            //             if (i.IndexOf(i[0]) == i.LastIndexOf(i[0]))
+            //                 return false;
+            //         }
+            //         return true;
+            //     }
+            // }
+            // 4.2 {
+            //     static bool IsArreyRich(string[] str)
+            //     {
+            //         foreach (var i in str)
+            //         {
+            //             if (!ischarhere(i, i[0]))
+            //                 return false;
+            //         }
+            //         return true;
+            //     }
+            // }
+            // 5 {
+            //     static string LngstString(string[] _str)
+            //     {
+            //         string k = _str[0];
+            //         for (int i = 1; i < _str.Length; i++)
+            //         {
+            //             k = (k.Length < _str[i].Length) ? _str[i] : k;
+            //         }
+            //         return k;
+            //     }
+            // }
+            // 6 {
+            //     static string StrUnion(string a, string b)
+            //     {
+            //         string str = "";
+            //         foreach (var i in a)
+            //         {
+            //             if (b.IndexOf(i) == -1)
+            //             {
+            //                 str += i;
+            //             }
+            //         }
+            //         return str;
+            //     }
+            // }
+            // 6.2 {
+            // static bool stringHasChar(string str, char a)
+            // {
+            //     if (str.IndexOf(a) != -1)
+            //         return true;
+            //     return false;
+            // }
+            // }
+            // 7 {
+            //     long Num = 0, Cnt = 0, All = 0;
+            //     do
+            //     {
+            //         System.Console.Write("enter a number : ");
+            //         Num = long.Parse(Console.ReadLine());
+            //         All++;
+            //         if (Num % 10 == 3)
+            //             Cnt++;
+            //     } while (Num != 500);
+            //     System.Console.WriteLine($"out of {All}, {Cnt} had 3 in thair Number of units.");
+            // }
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ 6LS/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace _6LS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* class Parabula
+            {
+                private int a;
+                private int b;
+                private int c;
+                public void parabula(int a, int b, int c)
+                {
+
+                }}
+            */
+            /*  
+               string[] strrey = new string[4];
+              strrey[0] = "abcydef";
+              strrey[1] = "abcdeya";
+              strrey[2] = "abcdeyda";
+              strrey[3] = "abcededya";
+
+              System.Console.WriteLine(/*sameEdgesAndCharyInStrrey(strrey));
+             */
+            /*
+             static int sameEdgesAndCharyInStrrey(string[] str)
+             {
+                int cnt = 0;
+                foreach (var i in str)
+                {
+                    if (i[0] == i[(i.Length - 1)] && i.IndexOf('y') != -1)
+                    {
+                        cnt++;
+                    }
+                }
+                return cnt;
+             }
+            */
+
+            /*
+             static int sameEdgesAndCharyInStrrey2(string[] str)
+              {
+                int cnt = 0;
+                foreach (var i in str)
+                {
+                    if (sameEdgesAndisTherey(i))
+                    {
+                        cnt++;
+                    }
+                }
+                return cnt;
+             }
+             static bool sameEdgesAndisTherey(string str)
+              {
+                if (str[0] == str[(str.Length - 1)] && str.IndexOf('y') != -1)
+                    return true;
+                return false;
+             }
+            */
+
+            /*
+             static int sameEdgesAndCharyInStrrey(string[] str)
+             {
+                int cnt = 0;
+                foreach (var i in str)
+                {
+                    if (sameEdges(i) && isTherey(i))
+                    {
+                        cnt++;
+                    }
+                }
+                return cnt;
+             }
+             static bool sameEdges(string str)
+             {
+                if (str[0] == str[(str.Length - 1)])
+                    return true;
+                return false;
+             }
+
+             static bool isTherey(string str)
+             {
+                if (str.IndexOf('y') != -1)
+                    return true;
+                return false;
+             }
+            */
+
+            /*
+              string str = "abcdefghighg";
+              System.Console.WriteLine(biggestTavInString(str));
+              static char biggestTavInString(string str)
+              {
+                  int Bigst = 0, Current = 0;
+                  char BestTav = '0';
+                  foreach (var i in str)
+                  {
+                      Current = AmountOfCharinString(str, i);
+                      if (Bigst < Current)
+                      {
+                          Bigst = Current;
+                          // למצוא את הגדול ביותר
+                          BestTav = i;
+                      }
+                  }
+                  return BestTav;
+              }
+              static int AmountOfCharinString(string a, char b)
+              {
+                  int cnt = 0;
+                  foreach (var k in a)
+                  {
+                      if (k == b)
+                          cnt++;
+                  }
+                  return cnt;
+              }
+            */
+            System.Console.ReadKey();
+
+
+
+        }
+    }
+}
+            /* 
+            var in c# is a multitype variable
+            first type that is assigned to the var is the type of the variable
+            the type can NOT be reassigned to a different type  
+            and can not be redeclaired
+            (lazy programer variable for not assigning a specific type)
+            */
